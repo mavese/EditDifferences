aa9ea8228de7825d014dd3fd30a85863ab99ef69
mavese <mavese@live.com>
2017-11-08 15:33:42 -0600

    .

--- editDifferences.py
+++ editDifferences.py
@@ -29,24 +29,35 @@ def playlist_transform(s,t,compareType="Song"):
     C=[]
     s.insert(0,[" ", " ", " "])
     t.insert(0,[" ", " ", " "])
-    C.append(range(len(t)))
+    C.append([(0,"")])
+    for i in range(1,len(s)-1):
+        C[0].append((i, "s"))
+    # C.append(range(len(t)))
     for i in range(len(s)-1):
-        C.append([i+1])
+        C.append([(i+1,"d")])
+    for lists in C:
+        print lists
     for i in range(1,len(s)):
         for j in range(1,len(t)):
             if(compareType == "Song"):
-                if s[i] == t[j]: c_match = C[i-1][j-1]
-                else: c_match = C[i-1][j-1]+1
+                if s[i] == t[j]: c_match = C[i-1][j-1][0]
+                else: c_match = C[i-1][j-1][0]+1
             elif(compareType == "Artist"):
-                if s[i][1] == t[j][1]: c_match = C[i-1][j-1]
-                else: c_match = C[i-1][j-1]+1
+                if s[i][1] == t[j][1]: c_match = C[i-1][j-1][0]
+                else: c_match = C[i-1][j-1][0]+1
             else:
-                if s[i][2] == t[j][2]: c_match = C[i-1][j-1]
-                else: c_match = C[i-1][j-1]+1
-            c_ins = C[i][j-1]+1
-            c_del = C[i-1][j]+1
-            c_min=min(c_match, c_ins, c_del)
-            C[i].append(c_min)
+                if s[i][2] == t[j][2]: c_match = C[i-1][j-1][0]
+                else: c_match = C[i-1][j-1][0]+1
+            c_ins = C[i][j-1][0]+1
+            c_del = C[i-1][j][0]+1
+            if(c_match <= c_ins and c_match <= c_del):
+                C[i].append((c_match, "a"))
+            elif(c_ins <= c_del):
+                C[i].append((c_ins, "s"))
+            else:
+                c[i].append((c_del, "d"))
+            # c_min=min(c_match, c_ins, c_del)
+            # C[i].append(c_min)
     for lists in C:
         print lists
     k = 0
@@ -55,34 +66,35 @@ def playlist_transform(s,t,compareType="Song"):
     down = False
     prev = 0
     path = ""
-    while k < len(C)-1 or l < len(C[k])-1:
-        if(k == len(C)-1 and l < len(C[k])-1):
-            l+=1
-        elif(k < len(C)-1 and l == len(C[k])-1):
-            k+=1
-            down = True
-        elif(C[k][l+1]<=C[k+1][l] and C[k][l+1]<=C[k+1][l+1]):
-            l+=1
-        elif(C[k+1][l]<=C[k][l+1] and C[k+1][l]<=C[k+1][l+1]):
-            k+=1
-            down = True
-        else:
-            k+=1
-            l+=1
-            angle = True
-        if(prev == C[k][l]):
-          print "keep {}".format(s[k][0])
-        elif(angle):
-            print "replace {} with {}".format(s[k][0],t[l][0])
-            angle = False
-        elif(down):
-            print "delete {}".format(s[k][0])
-            down = False
-        else:
-            print "insert {}".format(t[l][0])
-        prev = C[k][l]
-        path = path + "({},{})".format(k,l)
-    print path
+    # while k < len(C)-1 or l < len(C[k])-1:
+    #     if(k == len(C)-1 and l < len(C[k])-1):
+    #         l+=1
+    #     elif(k < len(C)-1 and l == len(C[k])-1):
+    #         k+=1
+    #         down = True
+    #     elif(C[k][l+1]<=C[k+1][l] and C[k][l+1]<=C[k+1][l+1]):
+    #         l+=1
+    #     elif(C[k+1][l]<=C[k][l+1] and C[k+1][l]<=C[k+1][l+1]):
+    #         k+=1
+    #         down = True
+    #     else:
+    #         k+=1
+    #         l+=1
+    #         angle = True
+    #     if(prev == C[k][l]):
+    #       print "keep {}".format(s[k][0])
+    #     elif(angle):
+    #         print "replace {} with {}".format(s[k][0],t[l][0])
+    #     elif(down):
+    #         print "delete {}".format(s[k][0])
+    #     else:
+    #         print "insert {}".format(t[l][0])
+    #     prev = C[k][l]
+    #     path = path + "({},{})".format(k,l)
+    #     down = False
+    #     angle = False
+    # print path
+    print C
     return C[i][j]
 
 
