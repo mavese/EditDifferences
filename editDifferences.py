520757848d64520c4267b5d33cb51f6b8152db04
Matteo Mantese <mavese@live.com>
2017-11-08 16:30:02 -0600

    Final optimization and algorithm redesign.

--- editDifferences.py
+++ editDifferences.py
@@ -27,16 +27,14 @@ def playlist_transform(s,t,compareType="Song"):
       s into playlist t.
     """
     C=[]
-    s.insert(0,[" ", " ", " "])
-    t.insert(0,[" ", " ", " "])
+    if(s[0] != [" ", " ", " "] and t[0] != [" ", " ", " "]):
+        s.insert(0,[" ", " ", " "])
+        t.insert(0,[" ", " ", " "])
     C.append([(0,"")])
-    for i in range(1,len(s)-1):
-        C[0].append((i, "s"))
-    # C.append(range(len(t)))
+    for i in range(1,len(s)+1):
+        C[0].append((i, "i"))
     for i in range(len(s)-1):
         C.append([(i+1,"d")])
-    for lists in C:
-        print lists
     for i in range(1,len(s)):
         for j in range(1,len(t)):
             if(compareType == "Song"):
@@ -53,49 +51,30 @@ def playlist_transform(s,t,compareType="Song"):
             if(c_match <= c_ins and c_match <= c_del):
                 C[i].append((c_match, "a"))
             elif(c_ins <= c_del):
-                C[i].append((c_ins, "s"))
+                C[i].append((c_ins, "i"))
             else:
-                c[i].append((c_del, "d"))
+                C[i].append((c_del, "d"))
             # c_min=min(c_match, c_ins, c_del)
             # C[i].append(c_min)
     for lists in C:
         print lists
-    k = 0
-    l = 0
-    angle = False
-    down = False
-    prev = 0
-    path = ""
-    # while k < len(C)-1 or l < len(C[k])-1:
-    #     if(k == len(C)-1 and l < len(C[k])-1):
-    #         l+=1
-    #     elif(k < len(C)-1 and l == len(C[k])-1):
-    #         k+=1
-    #         down = True
-    #     elif(C[k][l+1]<=C[k+1][l] and C[k][l+1]<=C[k+1][l+1]):
-    #         l+=1
-    #     elif(C[k+1][l]<=C[k][l+1] and C[k+1][l]<=C[k+1][l+1]):
-    #         k+=1
-    #         down = True
-    #     else:
-    #         k+=1
-    #         l+=1
-    #         angle = True
-    #     if(prev == C[k][l]):
-    #       print "keep {}".format(s[k][0])
-    #     elif(angle):
-    #         print "replace {} with {}".format(s[k][0],t[l][0])
-    #     elif(down):
-    #         print "delete {}".format(s[k][0])
-    #     else:
-    #         print "insert {}".format(t[l][0])
-    #     prev = C[k][l]
-    #     path = path + "({},{})".format(k,l)
-    #     down = False
-    #     angle = False
-    # print path
-    print C
-    return C[i][j]
+    pathList = []
+    while(i > 0 or j > 0):
+        if(C[i][j][1] == "a"):
+            if(C[i][j][0] == C[i-1][j-1][0]):
+                pathList.append("leave {} unchanged".format(s[i]))
+            else:
+                pathList.append("replace {} with {}".format(s[i],t[j]))
+            i-=1
+            j-=1
+        elif(C[i][j][1] == "i"):
+            pathList.append("insert {}".format(t[j]))
+            j-=1
+        else:
+            pathList.append("delete {}".format(s[i]))
+            i-=1
+    for i in xrange(len(pathList)):
+        print pathList.pop()
 
 
 if __name__=="__main__":
@@ -110,9 +89,9 @@ if __name__=="__main__":
     for song in b2:
         print song
     print "Comparing playlist similarity by song"
-    print playlist_transform(b1,b2)
+    playlist_transform(b1,b2)
     print "Comparing playlist similarity by genre"
-    print playlist_transform(b1,b2,"Genre")
+    playlist_transform(b1,b2,"Genre")
     print "Comparing playlist similarity by artist"
-    print playlist_transform(b1,b2,"Artist")
+    playlist_transform(b1,b2,"Artist")
     #include your own playlists below
